f2bdd818645a2f7752c13fa0b8304be53fb390db
Jason Stangroome <148754+jstangroome@users.noreply.github.com>
2025-09-17 08:39:44 +1000

    Add sanity test of JSON unmarshalling

---
+++ sanity_test.go
@@ -0,0 +1,30 @@
+package api
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestJsonUnmarshalNumberToString(t *testing.T) {
+	// Unmarshalling a JSON number into a struct field of type string fails
+	var outA struct{ X string }
+	err := json.Unmarshal([]byte(`{"X": 123}`), &outA)
+	assert.Error(t, err)
+	assert.Equal(t, "", outA.X)
+
+	// But unmarshalling into json.Number works
+	var outB struct{ X json.Number }
+	err = json.Unmarshal([]byte(`{"X": 123}`), &outB)
+	assert.NoError(t, err)
+	assert.Equal(t, "123", outB.X.String())
+}
+
+func TestJsonUnmarshalInt64(t *testing.T) {
+	// Unmarshalling a JSON number into a struct field of type string fails
+	var outA struct{ X int64 }
+	err := json.Unmarshal([]byte(`{"X": 123}`), &outA)
+	assert.NoError(t, err)
+	assert.Equal(t, int64(123), outA.X)
+}
